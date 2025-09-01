829784f98d151143b3030765b4914a9cdcd31ea3
Tomás Pacheco <tomaspedron@gmail.com>
2025-09-01 15:01:08 -0300

    Update Cannon.cs

--- Golden Bambu/Assets/Scripts/Cannon.cs
+++ Golden Bambu/Assets/Scripts/Cannon.cs
@@ -3,21 +3,28 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.UIElements;
 using UnityEngine.UIElements;
+
 public class Cannon : MonoBehaviour
 {
     public List<CuttableObject> objectsToSpawn;
     int next = 0;
     public Transform shootPoint;
     private List<CuttableObject> spawnedObjects = new List<CuttableObject>();
-    void Awake () {
-        for(int i = 0; i < objectsToSpawn.Count; i++) {
+
+    void Awake()
+    {
+        for (int i = 0; i < objectsToSpawn.Count; i++)
+        {
             CuttableObject obj = Instantiate(objectsToSpawn[i]);
             spawnedObjects.Add(obj);
             spawnedObjects[i].gameObject.SetActive(false);
         }
     }
+
     public Vector3 force;
-    public void SpawnNext() {
+
+    public void SpawnNext()
+    {
         var nextObject = spawnedObjects[next];
         nextObject.transform.position = shootPoint.position;
         nextObject.rb.Sleep();
@@ -26,19 +33,20 @@ public class Cannon : MonoBehaviour
 
 
         next++;
-        next = next%spawnedObjects.Count;
+        next = next % spawnedObjects.Count;
     }
 
     [CustomEditor(typeof(Cannon))]
     public class Cannon_Inspector : Editor
     {
-            public override void OnInspectorGUI() {
-                base.OnInspectorGUI();
-                if(GUILayout.Button("Spawn")) {
-                    var t = target as Cannon;
-                    t.SpawnNext();
-                }
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            if (GUILayout.Button("Spawn"))
+            {
+                var t = target as Cannon;
+                t.SpawnNext();
             }
-
+        }
     }
 }
