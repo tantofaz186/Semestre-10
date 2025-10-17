f359e202b82a49df04e6c014fb75811b97370bab
Tomás Pacheco <tomaspedron@gmail.com>
2025-10-17 11:19:52 -0300

    Make player slash with animation

--- Golden Bambu/Assets/Scripts/Player.cs
+++ Golden Bambu/Assets/Scripts/Player.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody), typeof(Collider))]
 public class Player : MonoBehaviour
 {
+    private static readonly int Attack = Animator.StringToHash("Attack");
     public float speed;
     private Rigidbody rb;
     private Collider col;
+    private Animator anim;
     private const float MAX_SPEED = 16;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
         col = GetComponent<Collider>();
+        anim = GetComponent<Animator>();
     }
 
     public void FixedUpdate()
@@ -20,5 +23,20 @@ public class Player : MonoBehaviour
         {
             rb.AddForce(transform.forward * speed, ForceMode.Acceleration);
         }
+    }
+
+    public void Start()
+    {
+        Sword.Instance.OnCut += TriggerCutAnimation;
+    }
+
+    public void OnDestroy()
+    {
+        Sword.Instance.OnCut -= TriggerCutAnimation;
+    }
+
+    private void TriggerCutAnimation(Plane plane)
+    {
+        anim.SetTrigger(Attack);
     }
 }
