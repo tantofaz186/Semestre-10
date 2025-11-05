6ddd392be764f494660356cebff95922ec922a50
Tomás Pedron Turchi Pacheco <42844869+tantofaz186@users.noreply.github.com>
2025-11-05 11:53:42 -0300

    Add character to main menu
    and bake lights in main menu and test scene

--- Golden Bambu/Assets/Scripts/_old/UIManager.cs
+++ Golden Bambu/Assets/Scripts/_old/UIManager.cs
@@ -4,15 +4,11 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public Button startButton;
-    public Button exitButton;
     public Slider masterVolumeSlider;
     public Slider musicVolumeSlider;
     public Slider sfxVolumeSlider;
     void Awake()
     {
-        startButton.onClick.AddListener(StartGame);
-        exitButton.onClick.AddListener(QuitGame);
         if (PlayerPrefs.HasKey("MasterVolume"))
         {
             masterVolumeSlider.value = PlayerPrefs.GetFloat("MasterVolume");
@@ -25,21 +21,15 @@ public class UIManager : MonoBehaviour
         }
     }
 
-    private void StartGame()
+    public void StartGame()
     {
         SceneManager.LoadScene("Level 1");
     }
 
-    private void QuitGame()
+    public void QuitGame()
     {
         Application.Quit();
     }
-    private void OnDisable()
-    {
-        startButton.onClick.RemoveListener(StartGame);
-        exitButton.onClick.RemoveListener(QuitGame);
-    }
-
     public void ChangeMasterVolume(float vol)
     {
         PlayerPrefs.SetFloat("MasterVolume", vol);
